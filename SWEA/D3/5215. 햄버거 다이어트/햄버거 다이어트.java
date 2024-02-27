d95a482eeec1eb881e16cef9822d231406597612
Yujachaa <114348883+yujachaa@users.noreply.github.com>
2024-02-27 16:59:31 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,427 ms, Memory: 90,712 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -2,23 +2,19 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Ingredient {
-	int score;
-	int cal;
-	
-	Ingredient () {};
-	
-	Ingredient (int score, int cal) {
-		this.score = score;
-		this.cal = cal;
-	}
-	
-}
 public class Solution {
 	//햄버거의 선호도 = 조합된 재료들의 맛에 대한 점수의 합
 	//입력 : 재료수, 제한 칼로리
 	//재료 맛점수, 재료 칼로리
 	//출력 : 제한 칼로리 이하 조합 중에서 가장 맛 점수가 높은 햄버거의 점수
+	
+	static int N; //재료 개수
+	static boolean[] sel; //재료의 사용 유무 넣/ 안넣
+	static int[] score; //맛점수
+	static int[] cal; //칼로리
+	static int best; //제한 칼로리 이하에서 맛점수 가장 높은 것
+	static int L; //제한 칼로리
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -28,47 +24,58 @@ public class Solution {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			
 			//재료수 N, 제한칼로리 L
-			int N = Integer.parseInt(st.nextToken());
-			int L = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			L = Integer.parseInt(st.nextToken());
 			
-			//재료 N개 입력
-			Ingredient[] data = new Ingredient[N];
+			sel = new boolean[N];
+			score = new int[N];
+			cal = new int[N];
+			
+			//N개 재료의 맛점수, 칼로리 입력
 			for(int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
-				int score = Integer.parseInt(st.nextToken());
-				int cal = Integer.parseInt(st.nextToken());
-				data[i] = new Ingredient(score, cal);
+				score[i] = Integer.parseInt(st.nextToken());
+				cal[i] = Integer.parseInt(st.nextToken());
 			}
 			
 			//최고 점수 저장
-			int best = 0;
-			
-			//재료들의 조합의 맛점수 합을 만들어 배열에 저장
-			//재료가 N개 -> 1 << N 가지 경우의 수
-			int[] combiScore = new int[1<<N];
-			int[] combiCal = new int[1<<N];
-						
-			//비트마스킹으로 만들자 2^N개 확인
-			for(int i = 0; i < (1 << N); i++) {
-				for(int j = 0; j < N; j++) { //N개의 재료 검사!!
-					if((i & (1 << j)) > 0) {
-						//비트마스킹 -> 들어있는 재료만 1
-						combiScore[i] += data[j].score;
-						combiCal[i] += data[j].cal;
-					}
-				}
-			}
-			
-			//조합별 맛점수, 칼로리 배열 같이 순회하면서, 
-			//칼로리 제한 이내이면서, 맛점수 최댓값인 것 찾기
-			for(int i = 0; i < (1 << N); i++) {
-				if(combiCal[i] <= L) {
-					best = Math.max(best, combiScore[i]);
-				}
-			}
+			best = 0;
+			//부분집합 만들어서 맛점수, 칼로리 계산해서 best 구하기
+			powerset(0);
+
+
 			//출력
 			System.out.printf("#%d %d\n", tc, best);
 		}//테스트케이스 끝
+		
+	}
+	//부분집합 -> 넣는다 / 안넣는다 두가지 경우
+	public static void powerset(int idx) {
+		//base case
+		if(idx == N) { //마지막까지 고려 완
+			int scoreSum = 0;
+			int calSum = 0;
+			for(int i = 0; i < N; i++) {
+				if(sel[i]) { //선택된 재료
+					scoreSum += score[i];
+					calSum += cal[i];
+				}
+			}
+			//제한 칼로리 이하면, 맛점수 best 갱신
+			if(calSum <= L) {
+				best = Math.max(best, scoreSum);
+			}
+			return;
+		}
+		
+		//recursive case
+		//골랐다
+		sel[idx] = true;
+		powerset(idx + 1);
+			
+		//안골랐다
+		sel[idx] = false;
+		powerset(idx + 1);
 		
 	}
 
