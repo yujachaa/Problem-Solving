ac6ed30a97f184741637ff713f749de9274292d9
Yujachaa <114348883+yujachaa@users.noreply.github.com>
2024-03-13 17:41:58 +0900

    [Gold V] Title: ABCDE, Time: 284 ms, Memory: 16876 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -0,0 +1,88 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    /*
+     * Q. ABCDE
+     * #문제요약
+     *     사람의 수 N, 친구 관계의 수 M이 주어지고
+     *     ABCDE 같은 친구관계가 존재하면 1 없으면 0을 출력한다.
+     * #메인접근법
+     *     1. dfs로 탐색해서 level이 4가 되면 존재하는 것
+     */
+    
+    static int N, M;
+    static ArrayList<ArrayList<Integer>> graph;
+    static boolean[] visited;
+    static boolean possible;
+    static int level;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        //0~N-1까지 초기화
+        graph = new ArrayList<>();
+        for(int i = 0; i < N; i++) {
+            graph.add(new ArrayList<>());
+        }
+        
+        //친구 관계 연결
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            
+            graph.get(v1).add(v2);
+            graph.get(v2).add(v1);
+        }
+        
+        possible = false;
+        visited = new boolean[N];
+
+        for(int i = 0; i < N; i++) {
+            level = 0;
+            dfs(i);
+            if(possible)
+            	break;
+        }
+        
+        if(possible) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+        
+    }
+    
+    //node : 현재 노드
+    private static void dfs(int node) {
+        //기저조건
+        if(level == 4) { //ABCDE 찾으면 탈출
+            possible = true;
+            return;
+        }
+        
+        //현재 노드에서 할 일
+        visited[node] = true; //방문체크
+                
+        //재귀부분
+        for(int i = 0; i < graph.get(node).size(); i++) {
+            int newNode = graph.get(node).get(i);
+            
+            if(!visited[newNode]) {
+                level++;
+                dfs(newNode);
+                level--;
+            }
+        }
+        visited[node] = false; //방문체크 초기화
+    }
+}
