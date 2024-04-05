aff259d81b4578bf2c696fdedae83a931dfabecb
Yujachaa <114348883+yujachaa@users.noreply.github.com>
2024-04-05 15:28:01 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 207 ms, Memory: 37,552 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
@@ -3,84 +3,77 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    //대각선 이동, 사각형을 그리며 출발한 카페로 돌아가야함
-    //같은 숫자 또 밟으면 X
-    //한 카페만 X
-    //왔던 길 되돌아가기 X
-    //가장 많이 먹을 수 있는 경로를 찾고, 그 때의 디저트 수 출력
-    //디저트 못 먹는 경우 -1 출력
-    
-    //1. 양옆, 밑에 2칸 있어야 사각형 만들 수 있음
-	//2. 이동할 때 방금 왔던 방향이랑 같은 방향 or 다음 방향으로만 갈 수 있음
-	
-    static int N;
-    static int[][] map;
-    static boolean[][] visited;
-    static int max;
-    static boolean[] dessert; //이미 나왔던 숫자인지 확인
-    
-    //대각선 이동 델타배열 : 좌하, 우하, 우상, 좌상 순서!!
-    static int[] dr = {1, 1, -1, -1};
-    static int[] dc = {-1, 1, 1, -1};
-    
-    
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int T = Integer.parseInt(br.readLine());
-        for(int tc = 1; tc <= T; tc++) {
-            N = Integer.parseInt(br.readLine());
-            map = new int[N][N];
-            
-            for(int i = 0; i < N; i++) {
-            	StringTokenizer st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < N; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-            
-            max = -1; //디폴트 -1
-            
-            //양 옆과 밑에 2칸이 있어야 사각형 가능
-            for(int r = 0; r < N - 2; r++) {
-            	for(int c = 1; c < N - 1; c++ ) {
-            		visited = new boolean[N][N];
-            		dessert = new boolean[101]; //1~100
-            		visited[r][c] = true;
-            		dessert[map[r][c]] = true;
-            		dfs(r,c,r,c,0,1);
-            		
-            	}
-            }
-            
-            System.out.printf("#%d %d\n", tc, max);
-        }
-    }
+	static int T, N, max; // 한변의 길이
+	static int[][] map;
+	static boolean[] dessert; // 담은 디저트의 숫자 체크
+	static boolean[][] visited; // 방문체크
+	static boolean flag; // 한바퀴 돌고 되돌아온건지 체크
 
-    //r,c:현재위치 sr,sc:초기위치 dir:이동방향 cnt:디저트개수
-	private static void dfs(int r, int c, int sr, int sc, int dir, int cnt) {
-		//이동할 때 방금 왔던 방향이랑 같은 방향 or 다음 방향으로만 갈 수 있음
-		for(int d = dir; d < 4; d++) {
-			int nr = r + dr[d];
-			int nc = c + dc[d];
+	// 이동방향 델타배열
+	// 우하, 좌하, 좌상, 우상
+	static int[] dr = { 1, 1, -1, -1 };
+	static int[] dc = { 1, -1, -1, 1 };
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			map = new int[N][N];
+
+			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			} // 디저트 정보 입력 끝
+
+			max = -1;
+
+			for (int r = 0; r < N - 2; r++) { // 범위 내에서 탐색
+				for (int c = 1; c < N - 1; c++) {
+					dessert = new boolean[101]; //1~100
+					visited = new boolean[N][N];
+					dessert[map[r][c]] = true; // 현재위치 숫자 체크
+					visited[r][c] = true; //현재위치 방문처리
+					dfs(r, c, r, c, 1, 0);
+				}
+			} // 모든 위치에서 확인 끝
+
+			System.out.printf("#%d %d\n", tc, max);
+		} // tc
+
+	}// main
+
+	// sr, sc:시작위치, row, col:현재위치, dir:이동방향
+	private static void dfs(int sr, int sc, int row, int col, int cnt, int dir) {
+
+		// 재귀 부분
+		for (int d = dir; d < 4; d++) {
+			// 같은방향으로 가거나 다음 방향으로 가기
+			int nr = row + dr[d];
+			int nc = col + dc[d];
 			
-			//범위 밖으로 나갈때
-			if(nr < 0 || nr >= N || nc < 0 || nc >= N)
-				continue;
-			//초기 위치로 돌아왔을때! - 조건 만족
-			if(nr == sr && nc == sc && cnt >= 4) {
+			if (nr < 0 || nr >= N || nc < 0 || nc >= N)
+				continue; // 범위 벗어나면 스킵
+			
+			if(cnt >= 4 && nr == sr && nc == sc) {
+				// 다음 위치가 한바퀴 돌고 다시 시작위치로 돌아온 거라면, 최댓값 갱신
 				max = Math.max(max, cnt);
 				return;
 			}
-			//안가본 곳이면서, 중복 아닌 곳으로 이동
+			
 			if(!visited[nr][nc] && !dessert[map[nr][nc]]) {
+				//방문하지 않은 곳이고, 담지 않은 숫자면 dfs
 				visited[nr][nc] = true;
 				dessert[map[nr][nc]] = true;
-				dfs(nr, nc, sr, sc, d, cnt + 1);
-				visited[nr][nc] = false;
+				dfs(sr, sc, nr, nc, cnt + 1, d);
+				//다음방향도 탐색해야되니까 안간척하기
 				dessert[map[nr][nc]] = false;
+				visited[nr][nc] = false;
 			}
 		}
-	
 	}
+
 }
