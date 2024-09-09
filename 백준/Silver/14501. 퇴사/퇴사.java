7d04ae35243dec7e4d2cefb2967c5c251ffcb322
Yunju Lee <114348883+yujachaa@users.noreply.github.com>
2024-09-10 00:57:55 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -4,52 +4,42 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	// 퇴사
+    static int N, answer;
+    static int[][] table;
 
-	// 1일~N일까지 최대한 많은 상담을 할 때의 최대 수익 구하기
 
-	static int N;
-	static int[] T, P;
-	static int max;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        table = new int[2][N+1]; //1~N까지 사용
+        for(int i = 1; i <= N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            table[0][i] = Integer.parseInt(st.nextToken());
+            table[1][i] = Integer.parseInt(st.nextToken());
+        }
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine()); // 날짜 수 1~15
-		T = new int[N + 1]; // 걸리는 기간 1~5
-		P = new int[N + 1]; // 받을 수 있는 금액 1~1000
+        findMax(1, 0);
 
-		for (int i = 1; i <= N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			T[i] = Integer.parseInt(st.nextToken());
-			P[i] = Integer.parseInt(st.nextToken());
-		}
+        System.out.println(answer);
+    }
 
-		max = 0;
+    public static void findMax(int idx, int price){
+        //기저조건
+        if(idx > N){
+            answer = Math.max(answer, price);
+            return;
+        }
 
-		findMax(1, 0);
-
-		System.out.println(max);
-	}
-
-	// idx : 현재날짜 상담 받을지 체크
-	private static void findMax(int idx, int sum) {
-		// 기저조건
-		if (idx >= N + 1) { 
-			max = Math.max(max, sum);
-			return;
-		}
-
-		if (idx + T[idx] <= N + 1)
-			//N일째에 1일짜리 일을 하면 N+1일까지 가니깐
-			//idx가 N+1가 될때까지는 상담을 끝마칠 수 있다!
-			// 상담을 끝마칠 수 있다면 오늘 날짜 상담 받는다!
-			findMax(idx + T[idx], sum + P[idx]);
-		else
-			// 오늘 상담 받으면 N을 지나버린다...!
-			findMax(idx + T[idx], sum);
-
-		// 오늘 그냥 스킵
-		findMax(idx + 1, sum);
-	}
-
+        //현재 idx에서 할 일
+        if(idx + table[0][idx] <= N + 1){
+        //현재 상담 하기
+            findMax(idx+table[0][idx], price + table[1][idx]);
+        }
+        else {
+            //퇴사해서 못하는 경우
+            findMax(idx+table[0][idx], price);
+        }
+            //상담 안하기
+            findMax(idx+1, price);
+    }
 }
