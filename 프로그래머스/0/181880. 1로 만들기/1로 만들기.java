a736284cf0af230aabcc90f1036eea2890ac9f35
Yunju Lee <114348883+yujachaa@users.noreply.github.com>
2024-11-11 21:50:04 +0900

    [level 0] Title: 1로 만들기, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        for(int i = 0; i < num_list.length; i++){
+            while(num_list[i] != 1){
+                num_list[i] /= 2;
+                answer++;
+            }
+        } return answer;
+    }
+}
